062a56b75ff1f863c29cd20e157321b71ab17af6
arshullah <ullaharshullah@gmail.com>
2024-11-06 15:51:02 +0530

    eventhandlingtodolist

--- frontend/src/app/event-handling/page.jsx
+++ frontend/src/app/event-handling/page.jsx
@@ -2,11 +2,27 @@
 
 import React from 'react'
 const EventHandling = () => {
+
+    const previewImange = (e) => {
+
+        const file = e.target.files[0];
+
+        const reader = new FileReader();
+
+        reader.onload = (data) => {
+            const img = new Image();
+            img.src = data.target.result;
+            document.body.appendChild(img);
+
+        }
+        reader.readAsDataURL(file);
+    }
+
     return (
         <div>
 
 
-            <div className='container mx-auto my-5 bg-black'>
+            <div className='container mx-auto my-5 '>
                 <h1 className='mt-4 font-bold text-3xl text-center'></h1>
 
                 <button
@@ -15,15 +31,19 @@ const EventHandling = () => {
 
                     className='p-3 border bg-gray-300'>Click Event</button>
 
-<input 
+                <input
 
-onChange={(e) =>{console.log(document.body.style.backgroundColor = e.target.value);}}
+                    onChange={(e) => { console.log(document.body.style.backgroundColor = e.target.value); }}
 
-className='mt-5 ' type="color" />
+                    className='mt-5 block' type="color" />
+
+                {/* file read krne ka tarika */}
+
+                <input type='file' onChange={previewImange} />
 
             </div>
         </div>
     )
 }
 
-export default EventHandling
+export default EventHandling;
