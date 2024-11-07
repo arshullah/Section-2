5d85158c78e7cafa3957510bac40bc32934c15ed
arshullah <ullaharshullah@gmail.com>
2024-11-07 15:54:05 +0530

    creating to do list

--- frontend/src/app/todolist/page.jsx
+++ frontend/src/app/todolist/page.jsx
@@ -6,8 +6,33 @@ const TodoList = () => {
 
     // let count = 0;
 
-    const [count, setCount] = useState(0);
-    
+    // const [count, setCount] = useState(0);   exmple hai
+
+
+    const [taskList, setTaskList] = useState([]);
+
+    // key press Event
+
+    const addNewTask = (e) => {
+
+        if(!e.target.value){
+            alert('please enter any task to add')
+        } return;
+
+        if (e.code === 'Enter') {
+            console.log(e.target.value);
+
+            const newTask = { text: e.target.value, completed: false };
+
+            setTaskList([newTask, ...taskList]);
+
+            e.target.value = '';
+        }
+
+
+
+    }
+
     return (
         <div className='bg-gray-300 h-screen pt-10'>
 
@@ -15,12 +40,15 @@ const TodoList = () => {
 
                 <div className='p-4 border-b-2'>
 
-                    <h1 className='text-3xl'>{count}</h1>
+                    {/* <h1 className='text-3xl'>{count}</h1> example hai */}
 
-                    <button onClick={ () => {setCount(count+1); console.log(count);
-                    }}>Add count</button>
+                    {/* <button onClick={ () => {setCount(count+1); console.log(count) }}>Add count</button> */}
 
-                    <input placeholder='Enter a Task to add in your todolist'
+                    <input
+
+                        onKeyDown={addNewTask}
+
+                        placeholder='Enter a Task to add in your todolist'
                         type="text"
                         className='px-3 py-2 rounded w-full bg-gray-200'
                     />
@@ -29,7 +57,20 @@ const TodoList = () => {
 
                 <div className='p-4'>
 
+                    {
 
+                        taskList.map((task, index) => {
+                            return <div key={index} className='rounded mb-4 p-4  border shadow'>
+
+                                <p className='text-xl '>{task.text}</p>
+
+                                <div className='mt-5 flex gap-5'>
+                                    <button className='bg-red-500 text-white rounded-full px-3 py-1'>Delete</button>
+                                    <button className='bg-blue-500 text-white rounded-full px-3 py-1'>Complete</button>
+                                </div>
+                            </div>
+                        })
+                    }
                 </div>
 
             </div>
